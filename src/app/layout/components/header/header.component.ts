878a3e8d1d727a81668090f28922424c38648358
Anuj Sharma <anuj11441@gmail.com>
2024-11-07 08:33:19 +0530

    7 nov start

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ApiService } from '../../../shared/services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
 
+export class HeaderComponent implements OnInit {
+  constructor(private ApiService: ApiService) { }
+  subCategories: any[] = [];
+
+  async ngOnInit() {
+    await this.fetchCategories();
+  }
+
+  async fetchCategories() {
+    await this.ApiService.getCategories().then(async (res) => {
+      debugger;
+      this.subCategories = res?.categories[0]?.subcategories;
+    })
+  }
 }
