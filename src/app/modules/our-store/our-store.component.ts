e3a3c831f6dbef6b40b5a1a73f00c16dbb563fac
Anuj Sharma <anuj11441@gmail.com>
2024-11-19 13:09:34 +0530

    19b nov

--- src/app/modules/our-store/our-store.component.ts
+++ src/app/modules/our-store/our-store.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ApiService } from '../../shared/services/api.service';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-our-store',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './our-store.component.html',
   styleUrl: './our-store.component.scss'
 })
-export class OurStoreComponent {
+export class OurStoreComponent implements OnInit {
+  constructor(private fb: FormBuilder,
+    private ApiService: ApiService,
+  ) {
 
+  }
+  stores: any = [];
+
+  async ngOnInit() {
+    await this.fetchStores();
+  }
+
+  async fetchStores() {
+    await this.ApiService.fetchStores().then((res) => {
+      this.stores = res
+    })
+  }
 }
